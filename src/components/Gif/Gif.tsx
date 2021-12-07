3c54bd410af6da565a125a3ce4ff0ec4cd963514
Remitekky <remitekkyhotmail.fr>
2021-12-08 00:05:17 +0100

    presentation

--- src/components/Gif/Gif.tsx
+++ src/components/Gif/Gif.tsx
@@ -3,19 +3,21 @@ import "./gif.css";
 import gif from "../../assets/images/presentation_gif.gif";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { useTranslation } from "react-i18next";
 
 const Gif = () => {
   useEffect(() => {
     AOS.init({});
   }, []);
+  const { t, i18n } = useTranslation("common");
   return (
     <div className="gif-component">
       <div data-aos="fade-down" data-aos-duration="800" className="gif-left-side">
-        <h1>Arcanis Boat Club</h1>
-        <p className="h1-class">10,000 generated boats waiting for their captain.</p>
-        <p>The Arcanis Boat Club is a series of 10,000 uniquely generated boats sailing the etehreum blockchain.</p>
-        <p>Each NFTs is unique and is based on the physical artwork of Arcanis Design.</p>
-        <p>Each boat's part are physically used on a real piece of art signed Arcanis Design !</p>
+        <h1>{t("presentation.title")}</h1>
+        <p className="h1-class">{t("presentation.subtitle")}</p>
+        <p>{t("presentation.desc1")}</p>
+        <p>{t("presentation.desc2")}</p>
+        <p>{t("presentation.desc3")}</p>
       </div>
       <div data-aos="fade-down" data-aos-duration="1000" className="gif-pyramid">
         <img src={gif} alt="" />
