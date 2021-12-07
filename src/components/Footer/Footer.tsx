fb193423534d3771626088b715b8a1b26decbda6
Remitekky <remitekkyhotmail.fr>
2021-12-08 00:48:39 +0100

    footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,29 +6,34 @@ import { ReactComponent as Instagram } from "../../assets/icons/instagram.svg";
 import footerLogo from "../../assets/icons/vandal_white.svg";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { useTranslation } from "react-i18next";
 
 const Footer: React.FC = () => {
   useEffect(() => {
     AOS.init({});
   }, []);
+  const { t, i18n } = useTranslation("common");
   return (
     <div className="footer">
       <div className="footer-left">
+        <a className="footer-left-a" href="https://arcanis.design/ ">
+          {t("footer.link.shop")}
+        </a>
+        <a className="footer-left-a" href="https://opensea.io/collection/arcanisboatclub ">
+          {t("footer.link.opensea")}
+        </a>
+        {/* <a className="footer-left-a" href="/">
+          {t("footer.link.terms")}
+        </a>
         <a className="footer-left-a" href="/">
-          Home
-        </a>
-        <a className="footer-left-a" href="#/terms">
-          Terms & Conditions
-        </a>
-        <a className="footer-left-a" href="/">
-          Smart Contract
-        </a>
+          {t("footer.link.smartcontract")}
+        </a> */}
       </div>
       <div className="footer-center">
         <div className="footer-logo">
           <img src={footerLogo} alt="Arcanis Boat Club Logo" />
         </div>
-        <span>©2021 Arcanis Boats Club. All rights reserved.</span>
+        <span>{t("footer.copyright")}</span>
       </div>
       <div className="footer-right">
         <div className=" footer-icons">
@@ -38,7 +43,7 @@ const Footer: React.FC = () => {
           <a href="https://www.instagram.com/arcanis.design/">
             <Instagram />
           </a>
-          <a href="#">
+          <a href="https://discord.gg/zmB9CHc2pp">
             <Discord />
           </a>
         </div>
