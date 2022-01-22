6439b155e6004ea839d27b323c60f38d2c0d9b97
Remitekky <remitekkyhotmail.fr>
2022-01-22 16:52:52 +0100

    navbar

--- components/Navbar/navbar.js
+++ components/Navbar/navbar.js
@@ -1,6 +1,9 @@
+import { CNavbar, CNavbarBrand, CNavbarNav, CNavbarToggler, CContainer, CCollapse, CNavItem, CNavLink } from '@coreui/react';
 import Link from 'next/link'
 import Image from 'next/image'
 import styles from "./navbar.module.css";
+// import '@coreui/coreui/dist/css/coreui.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default function Navbar() {
   return (
@@ -10,28 +13,29 @@ export default function Navbar() {
           src="/assets/images/logo.png"
           width="150px"
           height="150px"
+          alt='Arcanis Boat Club Logo'
         />
       </div>
       <nav className={styles.navigation}>
         <ul>
           <li>
             <Link href="/">
-              <a>Home</a>
+              <a><span>01.</span>Accueil</a>
             </Link>
           </li>
           <li>
             <Link href="/artist">
-              <a>Artist</a>
+              <a><span>02.</span>Artiste</a>
             </Link>
           </li>
           <li>
             <Link href="/roadmap">
-              <a>Roadmap</a>
+              <a><span>03.</span>Carte au trésor</a>
             </Link>
           </li>
           <li>
             <Link href="/team">
-              <a>Team</a>
+              <a><span>04.</span>Equipe</a>
             </Link>
           </li>
         </ul>
@@ -44,4 +48,50 @@ export default function Navbar() {
       </div>
     </div>
   )
+  // const { visible, setVisible } = false;
+  // return (
+  //   <>
+  //     <CNavbar expand="md">
+  //       <CContainer fluid>
+  //         <CNavbarToggler
+  //           aria-label="Toggle navigation"
+  //           aria-expanded={visible}
+  //           onClick={() => setVisible(!visible)}
+  //         />
+  //         <CCollapse className="navbar-collapse" visible={visible}>
+  //           <CNavbarBrand href="/">
+  //             <Image
+  //               src="/assets/images/logo.png"
+  //               width="100px"
+  //               height="100px"
+  //               alt='Arcanis Boat Club Logo'
+  //             />
+  //           </CNavbarBrand>
+  //           <CNavbarNav className="me-auto mb-2 mb-lg-0">
+  //             <CNavItem>
+  //               <Link href="/">
+  //                 <a>Home</a>
+  //               </Link>
+  //             </CNavItem>
+  //             <CNavItem>
+  //               <Link href="/artist">
+  //                 <a>Artist</a>
+  //               </Link>
+  //             </CNavItem>
+  //             <CNavItem>
+  //               <Link href="/roadmap">
+  //                 <a>Roadmap</a>
+  //               </Link>
+  //             </CNavItem>
+  //             <CNavItem>
+  //               <Link href="/team">
+  //                 <a>Team</a>
+  //               </Link>
+  //             </CNavItem>
+  //           </CNavbarNav>
+  //         </CCollapse>
+  //       </CContainer>
+  //     </CNavbar>
+  //   </>
+  // )
 }
