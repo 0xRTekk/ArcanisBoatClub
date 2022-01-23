1866fc5600f56e478c97b7594af891bf6c7fc3e2
Remitekky <remitekkyhotmail.fr>
2022-01-23 14:23:58 +0100

    banner

--- components/Banner/banner.js
+++ components/Banner/banner.js
@@ -1,28 +1,37 @@
-import styles from "./Banner.module.css";
+import Cta from '../Cta/cta'
+import styles from "./Banner.module.css"
 import bannerPicture from '../../public/assets/images/9.png'
 import Image from 'next/image'
 
 export default function Banner() {
   return(
     <div className={styles.banner}>
-      <div className={styles.intro}>
+
+      <div className={styles.intro_heading}>
         <h1>ARCANIS BOAT CLUB <span>10 000 bateaux attendant leur capitaine.</span></h1>
+
+      </div>
+
+      <div className={styles.intro_text}>
         <p>
-          Bienvenue à l’Arcanis Boat Club ! <br />
-          Ici vous allez découvrir les premières œuvres d’art génératif d’Arcanis. <br />
+          <strong>Bienvenue à l’Arcanis Boat Club !</strong> <br />
+          Ici vous allez découvrir les premières <strong>œuvres d’art génératif d’Arcanis</strong>. <br />
           Qu’est-ce que l’art génératif ? <br />
           C’est une pratique utilisant des algorithmes pour générer de manière autonome une œuvre.
         </p>
         <p>
-          Arcanis fait son entrée dans le monde de l’art digital par la grande porte et délivre une série de 10 000 bateaux exclusifs naviguant sur la blockchain Ethereum. <br />
+          Arcanis fait son entrée dans le <strong>monde de l’art digital</strong> par la grande porte et délivre une série de <strong>10 000 bateaux exclusifs</strong> naviguant sur la <strong>blockchain Ethereum</strong>. <br />
           Ils attendent leur capitaine.
         </p>
         <p>
-          Notre équipe vous permet de découvrir l’univers des NFT, quelle meilleure manière que d’y accoster à bord de votre propre navire ? <br />
-          Ces NFTs s’inspirent de l’art physique déjà reconnu d’Arcanis. <br />
-          Bientôt, de véritables bijoux numériques seront à votre disposition… Lequel aurez-vous la chance de posséder ? 
+          Notre équipe vous permet de découvrir <strong>l’univers des NFT</strong>, quelle meilleure manière que d’y accoster à bord de votre propre navire ? <br />
+          Ces NFTs s’inspirent de <strong>l’art physique déjà reconnu d’Arcanis</strong>. <br />
+          Bientôt, de véritables bijoux numériques seront à votre disposition… Lequel aurez-vous la chance de posséder ?
         </p>
-        <button className={styles.cta}>Voir sur OpenSea</button>
+        <Cta 
+          title='Mint'
+          link='https://opensea.io/collection/arcanisboatclub'
+        />
       </div>
 
         <div className={styles.picture}>
@@ -31,6 +40,7 @@ export default function Banner() {
             placeholder="blur"
             layout='responsive'
             priority
+            alt='Image NFT Arcanis Boat Club'
           />
       </div>
     </div>
---
+++ components/Cta/cta.js
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import styles from './Cta.module.css'
+
+export default function Cta({ title, link }) {
+  return (
+    <div>
+      <button className={styles.cssbuttons_iobutton}>
+      <Link href='/mint'>
+        <a>Mint à venir ! </a>
+      </Link>
+        <div className={styles.icon}>
+          <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor"></path></svg>
+        </div>
+      </button>
+
+    </div>
+  )
+}
