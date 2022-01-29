0f9974e4f28619bcdb58fe6ce24624e2f28050b8
Remitekky <remitekkyhotmail.fr>
2022-01-29 01:43:44 +0100

    road

--- components/Roadmap/roadmap.js
+++ components/Roadmap/roadmap.js
@@ -1,80 +1,101 @@
+import Image from 'next/image';
 import styles from "./Roadmap.module.css";
+import nft_badge from '../../public/assets/images/nft_badge_small.png';
 
 export default function Roadmap() {
   return (
-    <div className={styles.inner_wrapper}>
-      <div className="unknown-div">
-        <div className="vertical-line"></div>
-        <div className="roadmap">
-          <div>
-            <p className="line-line"></p>
-            <p className="diamond-raod">
-            </p>
-            <div data-aos-duration="700" data-aos="fade-left" style={{ display: "flex", alignItems: "center" }}>
-              <span>00%</span>
-              <h2>L&#39;Arcanis Boat Club</h2>
-            </div>
-            <p data-aos-duration="1000" data-aos="fade-left">
-              Votre pseudo sera rajouter sur l&#39;oeuvre d&#39;art final qui representera le ArcanisBoatClub qui fera l&#39;oeuvre d&#39;une grande video en plusieur partie sur youtube
-            </p>
-          </div>
-          <div>
-            <p className="diamond-raod">
-            </p>
-            <div data-aos-duration="700" data-aos="fade-left" style={{ display: "flex", alignItems: "center" }}>
-              <span>20%</span>
-              <h2>Radio LoFi</h2>
-            </div>
-            <p data-aos-duration="1000" data-aos="fade-left">
-              Creation de la radio Lofi qui diffusera H24 LES 10000 NFT et de la super musique
-            </p>
-          </div>
-          <div>
-            <p className="diamond-raod first-diamond">
-            </p>
-            <div data-aos-duration="700" data-aos="fade-left" style={{ display: "flex", alignItems: "center" }}>
-              <span>40%</span>
-              <h2>50 000€</h2>
-            </div>
-            <p data-aos-duration="1000" data-aos="fade-left">
-              Donnation de 50 000 € de cadeaux en tout genre (rolex,maroquinerie etc ) et 100 Pendentifs ABC 
-            </p>
-          </div>
-          <div>
-            <p className="diamond-raod">
-            </p>
-            <div data-aos-duration="700" data-aos="fade-left" style={{ display: "flex", alignItems: "center" }}>
-              <span>60%</span>
-              <h2>Merchandising</h2>
-            </div>
-            <p data-aos-duration="1000" data-aos="fade-left">
-              Creation du Merchandising
-            </p>
-          </div>
-          <div>
-            <p className="diamond-raod">
-            </p>
-            <div data-aos-duration="700" data-aos="fade-left" style={{ display: "flex", alignItems: "center" }}>
-              <span>80%</span>
-              <h2>Projet secret</h2>
-            </div>
-            <p data-aos-duration="1000" data-aos="fade-left">
-              On devoile le projet secret aux membres du ABC 
-            </p>
-          </div>
-          <div>
-            <p className="diamond-raod">
-            </p>
-            <div data-aos-duration="700" data-aos="fade-left" style={{ display: "flex", alignItems: "center" }}>
-              <span>100%</span>
-              <h2>100 000 €</h2>
-            </div>
-            <p data-aos-duration="1000" data-aos="fade-left">
-              Cn capitaine sera tiré au sort et gagnera 100 000€ de crypto de son choix
-            </p>
-          </div>
-        </div>
+    <section className={styles.roadmap}>
+
+      <div className={styles.line}>
+        <div className={styles.progression}></div>
       </div>
-    </div>
+
+      <div className={styles.start_line}>
+        <Image 
+          src={nft_badge}
+          className={styles.start_line_picture}
+          alt="bagde nft coming soon"
+        />
+      </div>
+
+      <article data-aos-duration="700" data-aos="fade-right" className={styles.card}>
+        <div className={styles.progression_dot}>
+          <div className={styles.inner_dot}></div>
+        </div>
+        <header className={styles.title}>
+          <span>00%</span>
+          <h2>L&#39;Arcanis Boat Club</h2>
+        </header>
+        <p className={styles.description}>
+          Votre pseudo sera rajouter sur l&#39;oeuvre d&#39;art final qui representera le ArcanisBoatClub qui fera l&#39;oeuvre d&#39;une grande video en plusieur partie sur youtube
+        </p>
+      </article>
+
+      <article data-aos-duration="700" data-aos="fade-left" className={styles.card}>
+        <div className={styles.progression_dot}>
+          <div className={styles.inner_dot}></div>
+        </div>
+        <header className={styles.title}>
+          <span>20%</span>
+          <h2>Radio LoFi</h2>
+        </header>
+        <p className={styles.description}>
+          Creation de la radio Lofi qui diffusera H24 LES 10000 NFT et de la super musique
+        </p>
+      </article>
+
+      <article data-aos-duration="700" data-aos="fade-right" className={styles.card}>
+        <div className={styles.progression_dot}>
+          <div className={styles.inner_dot}></div>
+        </div>
+        <header className={styles.title}>
+          <span>40%</span>
+          <h2>50 000€</h2>
+        </header>
+        <p className={styles.description}>
+          Donnation de 50 000 € de cadeaux en tout genre (rolex,maroquinerie etc ) et 100 Pendentifs ABC
+        </p>
+      </article>
+
+      <article data-aos-duration="700" data-aos="fade-left" className={styles.card}>
+        <div className={styles.progression_dot}>
+          <div className={styles.inner_dot}></div>
+        </div>
+        <header className={styles.title}>
+          <span>60%</span>
+          <h2>Merchandising</h2>
+        </header>
+        <p className={styles.description}>
+          Creation du Merchandising
+        </p>
+      </article>
+
+      <article data-aos-duration="700" data-aos="fade-right" className={styles.card}>
+        <div className={styles.progression_dot}>
+          <div className={styles.inner_dot}></div>
+        </div>
+        <header className={styles.title}>
+          <span>80%</span>
+          <h2>L&#39;Arcanis Boat Club</h2>
+        </header>
+        <p className={styles.description}>
+          On devoile le projet secret aux membres du ABC
+        </p>
+      </article>
+
+      <article data-aos-duration="700" data-aos="fade-left" className={styles.card}>
+        <div className={styles.progression_dot}>
+          <div className={styles.inner_dot}></div>
+        </div>
+        <header className={styles.title}>
+          <span>100%</span>
+          <h2>L&#39;Arcanis Boat Club</h2>
+        </header>
+        <p className={styles.description}>
+          Un capitaine sera tiré au sort et gagnera 100 000€ de crypto de son choix
+        </p>
+      </article>
+
+    </section>
   );
 }
--- pages/roadmap.js
+++ pages/roadmap.js
@@ -12,16 +12,15 @@ const meta = {
 export default function Roadmap() {
   return (
     <Layout roadmap>
-      <div className={styles.wrapper}>
+      <div data-aos-duration="700" data-aos="fade-down" className={styles.wrapper}>
         <h1>Carte au trésor</h1>
-        <section className={styles.roadmap}>
+        <section className={styles.description}>
           <p>
-            Chaque nouveau capitaine participe à la quête vers le trésor. Chaque pourcentage de vente (*des 10 000 bateaux) effectué débloque un nouveau palier de la carte. À chaque objectif de vente rempli, de nouveaux cadeaux seront disponibles sur l’Arcanis Boat Club. Plus la flotte sera grande et plus vous obtenez d’avantages. Et puisque la traversée se fait en équipe, vous devenez aussi membre du mouvement OTE : Own to Earn. 
+            Chaque nouveau capitaine participe à <strong>la quête vers le trésor</strong>. Chaque pourcentage de vente (*des 10 000 bateaux) effectué débloque un nouveau palier de la carte. À chaque objectif de vente rempli, de <strong>nouveaux cadeaux seront disponibles sur l’Arcanis Boat Club</strong>. Plus la flotte sera grande et plus vous obtenez d’avantages. Et puisque la traversée se fait en équipe, vous devenez aussi membre du <strong>mouvement OTE : Own to Earn</strong>. 
           </p>
-          <RoadmapComponent />
         </section>
-        <section className={styles.actions}>
-          <h2>Retrouvez les oeuvres d&#39;Arcanis sur ses différentes plateformes</h2>
+        <RoadmapComponent />
+        <section data-aos-duration="700" data-aos="fade-down" className={styles.actions}>
             <Socials />
         </section>
       </div>
